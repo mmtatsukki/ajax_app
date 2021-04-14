22dd0e5cecfa9d925d970fe638b0c75a0853f2b1
mmtatsukki <79450243+mmtatsukki@users.noreply.github.com>
2021-04-14 21:29:24 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username = ENV["BASIC_AUTH_USER"] && password == ENV[BASIC_AUTH_PASSWORD] #環境変数を読み込む記述に記載
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] #環境変数を読み込む記述に記載
     end
   end
 end
